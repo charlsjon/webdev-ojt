3cdf24511f75edafd6f64b5591129e578c4e56ea
charlsjon <charlsjonangeloerrazo@gmail.com>
2023-03-12 22:14:36 +0800

    card activity updated

--- cards/script.js
+++ cards/script.js
@@ -13,6 +13,21 @@ document.querySelector('.music-icon').addEventListener('click', function(){
 
 document.querySelector('.confirm-btn').addEventListener('.click', function(){
     var notesContainer = querySelector('.wrapper');
-    vat notes = document.createElement('div');
+    var notes = document.createElement('div');
+    var titleContainer = document.createElement('h2');
+    var dateContainer = document.createElement('p');
+    var descriptionContainer = document.createElement('p');
+    var inputTitle = document.querySelector('#title').value;
+    var inputDate = document.querySelector('#getDate').value;
+    var inputDescription = document.querySelector('#desc').value;
+
+    notesContainer.appendChild(notes).classlist.add('card');
+    // var cardData = document.querySelector('.card');
+    document.querySelector('.card').appendChild(titleContainer);
+    titleContainer.innerHTML = inputTitle;
+    document.querySelector('.card').appendChild(dateContainer);
+    dateContainer.innerHTML = inputDate;
+    document.querySelector('.card').appendChild(descriptionContainer);
+    descriptionContainer.innerHTML = inputDescription;
     
 })
